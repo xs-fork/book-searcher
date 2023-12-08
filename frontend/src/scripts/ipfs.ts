5ffc13287fa96819aea16284d4bb33b373f8368e
mrdoing <imshaohua@hotmail.com>
2023-12-08 09:58:25 +0800

    router

--- frontend/src/scripts/ipfs.ts
+++ frontend/src/scripts/ipfs.ts
@@ -15,7 +15,7 @@ export default async function getIpfsGateways() {
   } else {
     const ipfsGateways: string[] = JSON.parse(
       localStorage.getItem('ipfs_gateways') ||
-        '["https://linklife.me"]'
+        '["https://router.24hbook.com"]'
     );
     return ipfsGateways;
   }
